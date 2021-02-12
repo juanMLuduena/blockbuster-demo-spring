ac3d998ff6ce177b6292e90c66956359c68eb381
Juan Martin Ludueña <juanmluduena1@gmail.com>
2021-02-12 12:45:15 -0300

    Optimizando excepcions en Rent Service y Controller
    Se implemento un manejo de excepciones mas generalizado y limpio tanto en service como controller.
    Se agrego un logger a controller.

--- prueba spring/src/main/java/com/spring/controller/RentController.java
+++ prueba spring/src/main/java/com/spring/controller/RentController.java
@@ -1,13 +1,6 @@
 package com.spring.controller;
 
-import com.spring.exceptions.ClientDoesntExists;
-import com.spring.exceptions.EmployeeDoesntExists;
-import com.spring.exceptions.MovieAlreadyRented;
-import com.spring.exceptions.WrongData;
-import com.spring.model.Client;
-import com.spring.model.Employee;
-import com.spring.model.Movie;
-import com.spring.model.Rent;
+import com.spring.exceptions.*;
 import com.spring.service.ClientService;
 import com.spring.service.EmployeeService;
 import com.spring.service.MovieService;
@@ -19,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.logging.Level;
 
 @Log
@@ -37,53 +29,21 @@ public class RentController {
     }
 
     @GetMapping("/")
-    public List<Rent> getAll() {
-        return rentService.getAll();
+    public ResponseEntity<Object> getAll() {
+        return ResponseEntity.ok(rentService.getAll());
     }
+
 
     @PostMapping("/")
-    public void addRent(@RequestBody Rent newRent) throws WrongData {
-        rentService.addRent(newRent);
-    }
-
-    @PostMapping("/addByIds")
-    public ResponseEntity<Rent> addRent(@RequestParam Integer idMovie, @RequestParam int idClient, @RequestParam int idEmployee) throws WrongData {
-        ResponseEntity response = null;
-        try {
-            rentService.addRent(new Rent(new Employee(idEmployee), new Client(idClient), new Movie(idMovie)));
-            response = ResponseEntity.status(HttpStatus.CREATED).build();
-            log.log(Level.FINE, "Entidad renta creada sin problemas");
-        } catch (WrongData e) {
-            log.log(Level.WARNING, "Credenciales incorrectos al agregar renta");
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        } finally {
-            return response;
-        }
-    }
-
-
-    @PostMapping("/add")
-    public ResponseEntity<Rent> addRent(@RequestParam String title, @RequestParam String dniClient, @RequestParam String dniEmployee) {
-        ResponseEntity response = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    public ResponseEntity<Object> addRent(@RequestParam String title, @RequestParam String dniClient, @RequestParam String dniEmployee) {
+        ResponseEntity response;
         try {
             rentService.addRent(title, dniClient, dniEmployee);
             response = ResponseEntity.status(HttpStatus.CREATED).body("Carga de renta exitosa!");
             log.log(Level.FINE, "Entidad renta creada sin problemas");
-        } catch (MovieAlreadyRented me) {
-            log.log(Level.WARNING, "La película que trato de alquilar ya estaba alquilada");
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La película que trato de alquilar ya estaba alquilada");
-        } catch (ClientDoesntExists ce) {
-            log.log(Level.WARNING, "El cliente proporcionado no se encuentra en la base de datos");
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El cliente proporcionado no se encuentra en la base de datos");
-        } catch (EmployeeDoesntExists ee) {
-            log.log(Level.WARNING, "El empleado proporcionado no se encuentra en la base de datos");
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El empleado proporcionado no se encuentra en la base de datos");
-        } catch (WrongData e) {
-            log.log(Level.WARNING, "Credenciales incorrectos al agregar renta");
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Datos incorrectos proporcionados");
-        } catch(Exception e){
-            log.log(Level.WARNING, "Seguramente un error en MySQL");
-            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error desconocido");
+        } catch (BlockbusterDoesntExistsException | BlockbusterAlreadyExistsException | IllegalArgumentException e) {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            log.log(Level.WARNING, e.getMessage());
         }
         return response;
     }
--- prueba spring/src/main/java/com/spring/service/RentService.java
+++ prueba spring/src/main/java/com/spring/service/RentService.java
@@ -1,9 +1,6 @@
 package com.spring.service;
 
-import com.spring.exceptions.ClientDoesntExists;
-import com.spring.exceptions.EmployeeDoesntExists;
-import com.spring.exceptions.MovieAlreadyRented;
-import com.spring.exceptions.WrongData;
+import com.spring.exceptions.*;
 import com.spring.model.Client;
 import com.spring.model.Employee;
 import com.spring.model.Movie;
@@ -15,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.Objects.isNull;
 
 @Log
 @Service
@@ -33,35 +32,22 @@ public class RentService {
         this.movieService = movieService;
     }
 
-    public Rent addRent(Rent newRent) throws WrongData {
-        Rent rent = null;
-        try {
-            rent = rentRepository.save(newRent);
-        } catch (Exception e) {
-            e.getMessage();
-        }
-        return Optional.ofNullable(rent)
-                .orElseThrow(() -> new WrongData("Couldn't create a rent, please check the data provided"));
-    }
-
-    public Rent addRent(String title, String dniCliente, String dniEmployee) throws WrongData, ClientDoesntExists, EmployeeDoesntExists, MovieAlreadyRented {
+    public Rent addRent(String title, String dniCliente, String dniEmployee) throws BlockbusterDoesntExistsException, BlockbusterAlreadyExistsException {
         Rent rent = null;
         Movie movie = null;
         Client client = null;
         Employee employee = null;
 
+        if (isNull(title) || isNull(dniCliente) || isNull(dniEmployee))
+            throw new IllegalArgumentException("Los datos son invalidos.");
+
+        // Los 3 métodos que cargan los objetos tiran excepciones de validación
         movie = movieService.findByTitleAndRented(title, false);
-        //Los métodos getByDni tiran excepción
         client = clientService.getByDni(dniCliente);
         employee = employeeService.getByDni(dniEmployee);
-        if (movie == null) throw new MovieAlreadyRented("La película no esta disponible");
-        if ((employee == null) || (client == null)) throw new WrongData("Hubo un error interno");
-        try{
-            rent = rentRepository.save(new Rent(employee,client,movie));
-            movieService.rentMovie(movie);
-        }catch(Exception e){
-            e.getMessage();
-        }
+
+        rent = rentRepository.save(new Rent(employee, client, movie));
+        movieService.rentMovie(movie);
         return rent;
     }
 
