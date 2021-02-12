915e5a266ea1d2ca0d283a075b2d659404945d06
Juan Martin Ludueña <juanmluduena1@gmail.com>
2021-02-12 14:32:23 -0300

    Test ClientService getAllOk andando

--- prueba spring/src/test/java/com/spring/services/clientService/ClientServiceGetAllTest.java
+++ prueba spring/src/test/java/com/spring/services/clientService/ClientServiceGetAllTest.java
@@ -1,46 +1,47 @@
 package com.spring.services.clientService;
 
+import com.spring.exceptions.BlockbusterDoesntExistsException;
 import com.spring.model.Client;
 import com.spring.repository.ClientRepository;
 import com.spring.service.ClientService;
 import com.spring.service.MovieService;
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
 
-public class ClientServiceGetAllTest {
+class ClientServiceGetAllTest {
 
-    @Autowired
-    ClientService clientService;
-    @Autowired
+    @Mock
     MovieService movieService;
+
     @Mock
     ClientRepository clientRepository;
+
+    @InjectMocks
+    ClientService clientService;
 
     List<Client> clientList;
 
 
-    @Before("")
+    @BeforeEach
     public void setUp() {
-        clientService = new ClientService(clientRepository, movieService);
-        Client client1 = new Client(1, "Philip J.", "Fry", "123", true);
-        Client client2 = new Client(2, "Zapp", "Brannigan", "124", false);
-        clientList.add(client1);
-        clientList.add(client2);
+        openMocks(this);
     }
-/*
+
     @Test
-    public void getAllOkTest(){
-        when(clientService.getAll(null)).thenReturn(clientList);
+    void getAllOkTest() throws BlockbusterDoesntExistsException {
+        clientList = mock(List.class);
+        when(clientRepository.findAll()).thenReturn(clientList);
         List<Client> clientListResult = clientService.getAll(null);
-        assertEquals(2, clientListResult.size());
         verify(clientRepository, times(1)).findAll();
+        assertEquals(clientList, clientListResult);
     }
- */
+
 }
